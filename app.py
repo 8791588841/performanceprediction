0fb1fa956300e245fee949623492ae01441d8338
8791588841 <himanshu.bishtt@gmail.com>
2023-12-22 03:34:45 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -32,8 +32,10 @@ def predict_datapoint():
         print(pred_df) 
 
         predict_pipeline = PredictPipeline()
+        print("PredictPipeline")
         results = predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
+        print("Results :",results[0])
+        return render_template('home.html',results=int(results[0]))
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0")
